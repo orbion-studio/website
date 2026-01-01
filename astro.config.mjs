6283364c0eef0f35d87a978d632644836d479294
Lucas Silva <lucasnathanj@gmail.com>
2026-01-01 08:43:47 -0300

    feat: add sitemap, seo tags and remove astro favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
+  site: "https://orbion.studio/",
   vite: {
     plugins: [tailwindcss()],
-    server: {
-      allowedHosts: ["64d9d41c2df9.ngrok-free.app"],
-    },
   },
   devToolbar: {
     enabled: false,
